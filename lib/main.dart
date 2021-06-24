2d38493f13687aea6f79d11a8525b663f5ada216
Adrian Escobar <adrian080891@gmail.com>
2021-06-23 23:49:18 -0400

    Default structure
    default structure which can be changed a little more

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kimera_todo/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,16 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: Home(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Todo')));
   }
 }
---
+++ lib/widgets/appbar_widget.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends AppBar {
+  AppBarWidget() : super(title: Text('Todo'));
+}
