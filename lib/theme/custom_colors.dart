8b31541eb6d26b9e103e9295500b1e7528fbdbf2
Adrian Escobar <adrian080891@gmail.com>
2021-07-02 00:33:16 -0400

    Colors constants

---
+++ lib/theme/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color secundary_background = Color(0xFF031956);
+  static const Color background = Color(0xFF344fa1);
+  static const Color button = Color(0xFFe022f0);
+  static const Color menu_option_icon = Color(0xFF3e5697);
+  static const Color menu_option_text = Color(0xFFc5d6f4);
+}
