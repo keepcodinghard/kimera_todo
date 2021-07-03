2ebfccb266746b73c8215dd4d2dba46bee32e243
Adrian Escobar <adrian080891@gmail.com>
2021-07-03 13:37:52 -0400

    update custom colors

--- lib/theme/custom_colors.dart
+++ lib/theme/custom_colors.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class CustomColors {
   static const Color secundary_background = Color(0xFF031956);
   static const Color background = Color(0xFF344fa1);
-  static const Color button = Color(0xFFe022f0);
+  static const Color button = Color(0xFFeb05ff);
+  static const Color lines = Color(0xFFc11ce6);
   static const Color menu_option_icon = Color(0xFF3e5697);
   static const Color menu_option_text = Color(0xFFc5d6f4);
 }
